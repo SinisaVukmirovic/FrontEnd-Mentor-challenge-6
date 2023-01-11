1139e85f220aa25752d20ca7a4cebbfc5ac69c75
Sinisa Vukmirovic <sinisavukmirovic@gmail.com>
2023-01-12 00:38:48 +0100

    Refactored code

---
+++ src/data/galleryImages.js
@@ -0,0 +1,8 @@
+const galleryImages = [
+    '/images/mobile/image-gallery-milkbottles.jpg',
+    '/images/mobile/image-gallery-orange.jpg',
+    '/images/mobile/image-gallery-cone.jpg',
+    '/images/mobile/image-gallery-sugar-cubes.jpg'
+];
+
+export default galleryImages;
---
+++ src/data/socialLinks.js
@@ -0,0 +1,20 @@
+const socialLinks = [
+    {
+        src: '/images/icon-facebook.svg',
+        path: 'https://www.facebook.com/'
+    },
+    {
+        src: '/images/icon-instagram.svg',
+        path: 'https://www.instagram.com/'
+    },
+    {
+        src: '/images/icon-twitter.svg',
+        path: 'https://twitter.com/sajtowski'
+    },
+    {
+        src: '/images/icon-pinterest.svg',
+        path: 'https://www.linkedin.com/in/sinisa-vukmirovic-641a15124/'
+    },
+];
+
+export default socialLinks;
