039b0cdf7f569b22dd5edee571c344c684447371
Sinisa Vukmirovic <sinisavukmirovic@gmail.com>
2023-01-11 12:51:10 +0100

    Features component

---
+++ src/data/navData.js
@@ -0,0 +1,20 @@
+const navData = [
+    {
+        name: 'About',
+        href: '/about/'
+    },
+    {
+        name: 'Services',
+        href: '/services/'
+    },
+    {
+        name: 'Projects',
+        href: '/projects/'
+    },
+    {
+        name: 'Contact',
+        href: '/contact/'
+    }
+];
+
+export default navData;
