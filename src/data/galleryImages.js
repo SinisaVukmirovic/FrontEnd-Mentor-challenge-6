5b424d946c42d6c62114636d03921a971c0c9f51
Sinisa Vukmirovic <sinisavukmirovic@gmail.com>
2023-01-12 20:53:29 +0100

    Desktop styling

--- src/data/galleryImages.js
+++ src/data/galleryImages.js
@@ -1,8 +1,20 @@
 const galleryImages = [
-    '/images/mobile/image-gallery-milkbottles.jpg',
-    '/images/mobile/image-gallery-orange.jpg',
-    '/images/mobile/image-gallery-cone.jpg',
-    '/images/mobile/image-gallery-sugar-cubes.jpg'
-];
+    {
+        mob: '/images/mobile/image-gallery-milkbottles.jpg',
+        desk: '/images/desktop/image-gallery-milkbottles.jpg'
+    },
+    {
+        mob: '/images/mobile/image-gallery-orange.jpg',
+        desk: '/images/desktop/image-gallery-orange.jpg'
+    },
+    {
+        mob: '/images/mobile/image-gallery-cone.jpg',
+        desk: '/images/desktop/image-gallery-cone.jpg'
+    },
+    {
+        mob: '/images/mobile/image-gallery-sugar-cubes.jpg',
+        desk: '/images/desktop/image-gallery-sugarcubes.jpg'
+    },
+]
 
 export default galleryImages;
